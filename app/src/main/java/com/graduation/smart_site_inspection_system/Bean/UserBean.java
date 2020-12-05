e6cf346b9c79587968bf30e0122c7c1654ad0a53
Lin-Ningqiang <942242332@qq.com>
2020-12-05 20:12:49 +0800

    12.5__底部菜单栏；fragment搭建。

---
+++ app/src/main/java/com/graduation/smart_site_inspection_system/Bean/UserBean.java
@@ -0,0 +1,142 @@
+package com.graduation.smart_site_inspection_system.Bean;
+
+public class UserBean {
+
+    private int account;  //用户账户
+    private String name;  //用户名
+    private String password;  //用户密码
+    private int sex;  //性别
+    private int user_limit;  //权限
+    private String major;  //专业
+    private String academic;  //学历
+    private String nativeplace;  //籍贯
+    private String address;  //家庭住址
+    private int telephone;  //电话
+    private String job;  //职位
+    private int deleted;  //是否删除
+    private String create_time;  //创建时间
+    private String update_time;  //更新时间
+    private int version;  //版本
+
+    public int getAccount() {
+        return account;
+    }
+
+    public void setAccount(int account) {
+        this.account = account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getUser_limit() {
+        return user_limit;
+    }
+
+    public void setUser_limit(int user_limit) {
+        this.user_limit = user_limit;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getAcademic() {
+        return academic;
+    }
+
+    public void setAcademic(String academic) {
+        this.academic = academic;
+    }
+
+    public String getNativeplace() {
+        return nativeplace;
+    }
+
+    public void setNativeplace(String nativeplace) {
+        this.nativeplace = nativeplace;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(int deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(String update_time) {
+        this.update_time = update_time;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+
+}
---
+++ app/src/main/java/com/graduation/smart_site_inspection_system/Fragment_My/MyFragment.java
@@ -0,0 +1,27 @@
+package com.graduation.smart_site_inspection_system.Fragment_My;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.graduation.smart_site_inspection_system.R;
+
+public class MyFragment extends Fragment {
+    private Handler handler;
+
+    public MyFragment(Handler handler) {
+        this.handler = handler;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+}
